29b5728fa137b87d1db4cb53241e964ed85041a3
BruvLio <lionel.mbayu@gmail.com>
2023-05-14 15:31:54 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -1,1 +1,9 @@
 // build your server here and require it from index.js
+const express = require("express");
+const helmet = require("helmet");
+const router = require("./router");
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use("./api", router);
