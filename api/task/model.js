0ba599a9a6e6613b150628642b5112b07ed95c7b
BruvLio <lionel.mbayu@gmail.com>
2023-05-15 15:33:43 -0500

    wip

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,21 @@
 // build your `Task` model here
+const db = require("../../data/dbConfig");
+
+const get = async () => {
+  return db("tasks");
+};
+
+const getTaskById = async (task_id) => {
+  return db("tasks").where("task_id", task_id).first();
+};
+
+const insert = async (task) => {
+  const [id] = await db("tasks").insert(task);
+  return getTaskById(id);
+};
+
+module.exports = {
+  get,
+  getTaskById,
+  insert,
+};
