6dc0c278e9a527132e6771984f4783447f5ff517
BruvLio <lionel.mbayu@gmail.com>
2023-05-15 12:58:42 -0500

    wip

--- data/seeds/03-tasks.js
+++ data/seeds/03-tasks.js
@@ -4,10 +4,45 @@
  */
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex("table_name").del();
-  await knex("table_name").insert([
-    { id: 1, colName: "rowValue1" },
-    { id: 2, colName: "rowValue2" },
-    { id: 3, colName: "rowValue3" },
+  await knex("tasks").del();
+  await knex("tasks").insert([
+    {
+      task_description: "rowValue1",
+      task_notes: "",
+      task_completed: false,
+      project_id: 1,
+    },
+    {
+      task_description: "rowValue1",
+      task_notes: "",
+      task_completed: false,
+      project_id: 1,
+    },
+    {
+      task_description: "rowValue1",
+      task_notes: "",
+      task_completed: false,
+      project_id: 1,
+    },
+    {
+      task_description: "rowValue1",
+      task_notes: "",
+      task_completed: false,
+      project_id: 1,
+    },
   ]);
 };
+
+// .createTable("tasks", (table) => {
+//   table.increments("task_id");
+//   table.string("task_description").notNullable();
+//   table.string("task_notes");
+//   table.boolean("task_completed").defaultTo(false);
+//   table
+//     .integer("project_id")
+//     .notNullable()
+//     .references("project_id")
+//     .inTable("projects")
+//     .onDelete("RESTRICT")
+//     .onUpdate("RESTRICT");
+// })
