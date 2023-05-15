23811cc261bb54ed730b4e7b43811ce935e2f652
BruvLio <lionel.mbayu@gmail.com>
2023-05-15 12:26:38 -0500

    seeded projects table

--- data/seeds/01-projects.js
+++ data/seeds/01-projects.js
@@ -1,13 +1,24 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex("projects").insert([
+    {
+      project_name: "rowValue1",
+      project_description: "",
+      project_completed: false,
+    },
+    {
+      project_name: "rowValue1",
+      project_description: "",
+      project_completed: false,
+    },
+    {
+      project_name: "rowValue1",
+      project_description: "",
+      project_completed: false,
+    },
   ]);
 };
